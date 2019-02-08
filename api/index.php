c745e8458ee51c65467df64c3107d97240478d8e
Hari Om Thapa <40119660+thapadotin@users.noreply.github.com>
2019-02-08 15:30:14 +0530

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>API | Home</title>
+</head>
+<body>
+	<div id="wrapper">
+		<div id="data"></div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="function.js"></script>
+</body>
+</html>
